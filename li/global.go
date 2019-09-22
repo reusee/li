f703f6337d2c6d15ae87e409c7ca240865ed7561
reus <reusee@gmail.com>
2019-09-22 14:43:14 +0800

    add Init2 type

--- li/global.go
+++ li/global.go
@@ -8,17 +8,52 @@ type Derive func(inits ...interface{})
 
 type Init func() any
 
+type Init2 any
+
+var init2Type = reflect.TypeOf((*Init2)(nil)).Elem()
+
 func NewGlobal(fns ...any) Scope {
-	provide := new(Provide)
 	var inits []interface{}
-	v := reflect.ValueOf(provide)
-	for i := 0; i < v.NumMethod(); i++ {
-		fn := v.Method(i).Interface()
+	var protoInit2s []any
+
+	processFunc := func(fnValue reflect.Value) {
+		fn := fnValue.Interface()
 		if initFunc, ok := fn.(func() Init); ok {
 			fn = initFunc()
+			if fn == nil {
+				return
+			}
+		} else if t := fnValue.Type(); t.NumOut() == 1 && t.Out(0) == init2Type {
+			protoInit2s = append(protoInit2s, fn)
+			return
 		}
 		inits = append(inits, fn)
 	}
-	inits = append(inits, fns...)
-	return NewScope(inits...)
+
+	provide := new(Provide)
+	v := reflect.ValueOf(provide)
+	for i := 0; i < v.NumMethod(); i++ {
+		processFunc(v.Method(i))
+	}
+
+	for _, fn := range fns {
+		processFunc(reflect.ValueOf(fn))
+	}
+
+	scope := NewScope(inits...)
+
+	var init2s []any
+	for _, proto := range protoInit2s {
+		rets := scope.Call(proto)
+		for _, ret := range rets {
+			fn := ret.Interface()
+			if fn == nil {
+				continue
+			}
+			init2s = append(init2s, fn)
+		}
+	}
+	scope = scope.Sub(init2s...)
+
+	return scope
 }
