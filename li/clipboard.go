60f7c5c391db781fb98600c2fc0c940b7fd334df
reus <reusee@gmail.com>
2019-10-27 12:17:06 +0800

    cache Clip string

--- li/clipboard.go
+++ li/clipboard.go
@@ -5,45 +5,50 @@ import "strings"
 type Clip struct {
 	Moment *Moment
 	Range  Range
+	str    *string
 }
 
 func (c *Clip) String(scope Scope) string {
-	if c.Range.End.Before(c.Range.Begin) {
-		c.Range.End, c.Range.Begin = c.Range.Begin, c.Range.End
-	}
-
-	buf := new(strings.Builder)
-	lineNum := c.Range.Begin.Line
-
-	line := c.Moment.GetLine(scope, lineNum)
-	if line == nil {
-		return ""
-	}
-	begin := line.Cells[c.Range.Begin.Cell].ByteOffset
-	end := len(line.content)
-	if c.Range.End.Line == c.Range.Begin.Line {
-		end = line.Cells[c.Range.End.Cell].ByteOffset
-	}
-	buf.WriteString(line.content[begin:end])
-	lineNum++
-
-	for {
-		if lineNum >= c.Moment.NumLines() ||
-			lineNum > c.Range.End.Line {
-			break
+	if c.str == nil {
+		if c.Range.End.Before(c.Range.Begin) {
+			c.Range.End, c.Range.Begin = c.Range.Begin, c.Range.End
 		}
-		line = c.Moment.GetLine(scope, lineNum)
-		if lineNum == c.Range.End.Line {
-			buf.WriteString(
-				line.content[:line.Cells[c.Range.End.Cell].ByteOffset],
-			)
-		} else {
-			buf.WriteString(line.content)
+
+		buf := new(strings.Builder)
+		lineNum := c.Range.Begin.Line
+
+		line := c.Moment.GetLine(scope, lineNum)
+		if line == nil {
+			return ""
 		}
+		begin := line.Cells[c.Range.Begin.Cell].ByteOffset
+		end := len(line.content)
+		if c.Range.End.Line == c.Range.Begin.Line {
+			end = line.Cells[c.Range.End.Cell].ByteOffset
+		}
+		buf.WriteString(line.content[begin:end])
 		lineNum++
-	}
 
-	return buf.String()
+		for {
+			if lineNum >= c.Moment.NumLines() ||
+				lineNum > c.Range.End.Line {
+				break
+			}
+			line = c.Moment.GetLine(scope, lineNum)
+			if lineNum == c.Range.End.Line {
+				buf.WriteString(
+					line.content[:line.Cells[c.Range.End.Cell].ByteOffset],
+				)
+			} else {
+				buf.WriteString(line.content)
+			}
+			lineNum++
+		}
+
+		str := buf.String()
+		c.str = &str
+	}
+	return *c.str
 }
 
 func NewClipFromSelection(
